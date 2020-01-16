334752996c06e6ec172d55500f76d3b91e09eb66
praathul <athul.p.aux@go-jek.com>
2020-01-17 02:12:54 +0530

    Modify to send data to api

--- src/application_front/core.cljs
+++ src/application_front/core.cljs
@@ -1,18 +1,45 @@
 (ns application-front.core
-    (:require
-      [reagent.core :as r]))
+  (:require
+    [reagent.core :as r]))
 
 ;; -------------------------
 ;; Views
 
-(defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+(def text-box {
+               :height    40
+               :width     350
+               :font-size 20
+               })
+
+(defn sign-up []
+  (do
+    (-> (js/fetch "http://localhost:8080/sign-up" (clj->js {
+                                                  :method  "POST"
+                                                  :headers {
+                                                             :content-type "application/json"
+                                                            }
+                                                  :body    (.stringify js/JSON (clj->js {:username (-> js/document
+                                                                                   (.getElementById "username")
+                                                                                   (.-value))
+                                                                     :password (-> js/document
+                                                                                   (.getElementById "password")
+                                                                                   (.-value))
+                                                                     }))
+                                                  }))
+        (.then (fn [response] (println "user is added to the server"))))))
+
+(defn signup-page []
+  [:div [:h2 "This is a sample application"]
+   [:input {:type "text" :id "username" :placeholder "Enter Username*" :style text-box}]
+   [:input {:type "password" :id "password" :placeholder "Enter Username*" :style text-box}]
+   [:button {:id "submit-button" :on-click sign-up :style text-box} "Sign Up"]
+   ])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (r/render [signup-page] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
