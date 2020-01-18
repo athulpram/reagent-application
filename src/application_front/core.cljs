adb463dda7b46303e6d08fa999ed4baa843adb1d
praathul <athul.p.aux@go-jek.com>
2020-01-18 17:31:37 +0530

    Add method called fetch to reuse

--- src/application_front/core.cljs
+++ src/application_front/core.cljs
@@ -11,22 +11,24 @@
                :font-size 20
                })
 
+(defn fetch [url, method, headers, payload]
+  (js/fetch url (clj->js {
+                          :method  method
+                          :headers headers
+                          :body    (.stringify js/JSON (clj->js payload))
+                          })))
+
 (defn sign-up []
-  (do
-    (-> (js/fetch "http://localhost:8080/sign-up" (clj->js {
-                                                  :method  "POST"
-                                                  :headers {
-                                                             :content-type "application/json"
-                                                            }
-                                                  :body    (.stringify js/JSON (clj->js {:username (-> js/document
-                                                                                   (.getElementById "username")
-                                                                                   (.-value))
-                                                                     :password (-> js/document
-                                                                                   (.getElementById "password")
-                                                                                   (.-value))
-                                                                     }))
-                                                  }))
-        (.then (fn [response] (println "user is added to the server"))))))
+  (-> (fetch "http://localhost:8080/sign-up" "POST" {
+                                                     :content-type "application/json"
+                                                     } {:username (-> js/document
+                                                                      (.getElementById "username")
+                                                                      (.-value))
+                                                        :password (-> js/document
+                                                                      (.getElementById "password")
+                                                                      (.-value))
+                                                        })
+      (.then (fn [response] (println "user is added to the server")))))
 
 (defn signup-page []
   [:div [:h2 "This is a sample application"]
